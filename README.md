b67e4eccee14671204d7136ea4a3fc668ad1c02e
Jung <Jung@naver.com>
2024-09-19 11:10:24 +0900

    weather todo 소스

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # project1-2024
 2024-2학기 캡스톤프로젝트 수업
 
+개발순서
+1. 소스수정
+2. 소스 저장
+3. 스테이지 
+4. 커밋에 푸쉬
+5. 커밋메시지
+
+
+
+git 설정
+git config --global user.name "Jung"
+git config --global user.email "Jung@naver.com"
+
+
 2024-9-19 깃허브연동실습
 로컬에서 편집함
---
+++ todo/todo.js
@@ -0,0 +1,17 @@
+function    addItem(){
+
+    alert("addItem 함수가 호출되었습니다")
+
+    list = document.getElementById('todolist');
+    todo = document.getElementById('item');
+
+    let listitem = document.createElement('li');
+    let xbtn = document.createElement('button');
+
+    listitem.className = 'list-group-item list-group-item-action list-group-item-warning';
+    xbtn.className = 'close';
+    xbtn.innerHTML = '&items;'
+
+    listitem.innerText = todo.value;
+    list.appendChild(listitem);
+ }
